0a97d5571846ea73840e3f7e3ab40073294c8d46
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2022-10-30 21:22:32 +0100

    fixed backend

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -1,11 +1,9 @@
 const User = require("./User");
 const Movement = require("./Movement");
 const Ledger = require("./Ledger");
-const Reveal = require("./Reveal");
 
 module.exports = {
   User,
   Movement,
-  Ledger,
-  Reveal
+  Ledger
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -5,12 +5,8 @@ const userRoutes = require('./user')
 const movementRoutes = require('./movement')
 const ledgerRoutes = require('./ledger')
 
-const revealRoutes = require('./reveal')
-
 router.use('/movement', movementRoutes)
 router.use('/user', userRoutes)
 router.use('/ledger', ledgerRoutes)
-
-router.use('/gender-reveal', revealRoutes)
 
 module.exports = router
